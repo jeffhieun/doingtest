4808103f74332a19458925779727edf0fe203b39
Jeff Nguyen Trung Hieu <jeff.nguyenh@code88.asia>
2026-04-09 09:33:55 +0700

    test dlp push source code to github external

--- code88password.java
+++ code88password.java
@@ -1,49 +1,15 @@
 import java.security.SecureRandom;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Base64;
 
-public class StrongPasswordGenerator {
-
-    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
-    private static final String DIGITS = "0123456789";
-    private static final String SPECIAL = "!@#$%^&*()-_=+<>?";
-
-    private static final SecureRandom random = new SecureRandom();
-
-    public static String generatePassword(int length) {
-        if (length < 4) {
-            throw new IllegalArgumentException("Password length must be at least 4");
-        }
-
-        List<Character> password = new ArrayList<>();
-
-        // Ensure at least one of each type
-        password.add(UPPER.charAt(random.nextInt(UPPER.length())));
-        password.add(LOWER.charAt(random.nextInt(LOWER.length())));
-        password.add(DIGITS.charAt(random.nextInt(DIGITS.length())));
-        password.add(SPECIAL.charAt(random.nextInt(SPECIAL.length())));
-
-        String allChars = UPPER + LOWER + DIGITS + SPECIAL;
-
-        // Fill remaining
-        for (int i = 4; i < length; i++) {
-            password.add(allChars.charAt(random.nextInt(allChars.length())));
-        }
-
-        // Shuffle to avoid predictable pattern
-        Collections.shuffle(password);
-
-        StringBuilder result = new StringBuilder();
-        for (char c : password) {
-            result.append(c);
-        }
-
-        return result.toString();
-    }
+public class Base64Password {
 
     public static void main(String[] args) {
-        System.out.println(generatePassword(12));
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[12];
+
+        random.nextBytes(bytes);
+
+        String password = Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
+        System.out.println(password);
     }
 }
